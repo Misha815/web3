8237ed0da8f05ff60d7267179a04d165edacbc18
Misha815 <msha.stepnov.01@mail.ru>
2024-02-09 16:06:03 +0300

    first commit

---
+++ js/popup.js
@@ -0,0 +1,34 @@
+const popup = document.getElementById('containerPopup');
+const openPopup = document.querySelector('.openPopupBtn');
+const closePopup = document.querySelector('.send-popup');
+
+function openModalAndLockScroll() {
+    popup.showModal();
+    document.body.classList.add('scroll-lock');
+
+}
+
+function returnScroll() {
+    document.body.classList.remove('scroll-lock')
+}
+
+function close() {
+    popup.close()
+
+}
+
+function closeOnClick({ currentTarget, target }) {
+    const popup = currentTarget
+    const clickDrop = target === popup
+    if (clickDrop) {
+        close();
+        returnScroll();
+    }
+}
+
+popup.addEventListener('click', closeOnClick)
+openPopup.addEventListener('click', openModalAndLockScroll)
+closePopup.addEventListener('click', (event) => {
+    event.stopPropagation()
+    close()
+})
